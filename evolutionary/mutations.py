64a5c776e15e5231f5e564b20816de13d1c6046b
bmys <bmys94@gmail.com>
2019-11-04 17:23:37 +0100

    Change Swap_mutation to decorator, fix mutation_swap_operation

--- evolutionary/mutations.py
+++ evolutionary/mutations.py
@@ -1,55 +1,45 @@
 from typing import Sequence, Callable, Set
 from sequences.manipulation import random_change_at_index, \
     random_swap, \
-    get_random_index, \
     change_at_index, \
-    get_random_indexes, \
     random_enumerations, \
     change_at_indexes
 from random import randint
 
 
-def mutation_swap(sequence: Sequence, mutation_function: Callable) -> Sequence:
-    (first_index, first_instruction), (second_index, second_instruction) = random_enumerations(sequence, 2)
-    # mutated_first, mutated_second = map(mutation_function, (first_instruction, second_instruction))
-    mutated_first, mutated_second = mutation_function(first_instruction, second_instruction)
-    return change_at_indexes(sequence, (first_index, mutated_first), (second_index, mutated_second))
+def mutation_swap(mutation_function):
+    def inner(sequence):
+        (first_index, first_instruction), (second_index, second_instruction) = random_enumerations(sequence, 2)
+        mutated_first, mutated_second = mutation_function(first_instruction, second_instruction)
+        return change_at_indexes(sequence, (first_index, mutated_first), (second_index, mutated_second))
+    return inner
 
 
 def mutation_swap_instructions(program: Sequence) -> Sequence:
     return random_swap(program)
 
 
-def mutation_swap_operation(program: Sequence) -> Sequence:
-
-    def swap_operations(seq_1, seq_2):
-        mutated_first_instruction = seq_2[0] + seq_1[1:]
-        mutated_second_instruction = seq_1[0] + seq_2[1:]
-        return mutated_first_instruction, mutated_second_instruction
-
-    return mutation_swap(program, swap_operations)
+@mutation_swap
+def mutation_swap_operation(seq_1, seq_2):
+    mutated_first_instruction = change_at_index(seq_1, 0, seq_2[0])
+    mutated_second_instruction = change_at_index(seq_2, 0, seq_1[0])
+    return mutated_first_instruction, mutated_second_instruction
 
 
-def mutation_swap_operands(program: Sequence) -> Sequence:
-
-    def swap_operands(seq_1, seq_2):
-        mutated_first_instruction = change_at_index(seq_2, 0, seq_1[0])
-        mutated_second_instruction = change_at_index(seq_1, 0, seq_2[0])
-        return mutated_first_instruction, mutated_second_instruction
-
-    return mutation_swap(program, swap_operands)
+@mutation_swap
+def mutation_swap_operands(seq_1, seq_2):
+    mutated_first_instruction = change_at_index(seq_2, 0, seq_1[0])
+    mutated_second_instruction = change_at_index(seq_1, 0, seq_2[0])
+    return mutated_first_instruction, mutated_second_instruction
 
 
-def mutation_swap_operand(program: Sequence) -> Sequence:
-
-    def swap_operand(seq_1, seq_2):
-        first_operand_index = randint(1, len(seq_1) - 1)
-        second_operand_index = randint(1, len(seq_2) - 1)
-        mutated_first_instruction = change_at_index(seq_1, first_operand_index, seq_2[second_operand_index])
-        mutated_second_instruction = change_at_index(seq_2, second_operand_index, seq_1[first_operand_index])
-        return mutated_first_instruction, mutated_second_instruction
-
-    return mutation_swap(program, swap_operand)
+@mutation_swap
+def mutation_swap_operand(seq_1, seq_2) -> Sequence:
+    first_operand_index = randint(1, len(seq_1) - 1)
+    second_operand_index = randint(1, len(seq_2) - 1)
+    mutated_first_instruction = change_at_index(seq_1, first_operand_index, seq_2[second_operand_index])
+    mutated_second_instruction = change_at_index(seq_2, second_operand_index, seq_1[first_operand_index])
+    return mutated_first_instruction, mutated_second_instruction
 
 
 def mutation_change_operand(program: Sequence, operands: Set) -> Sequence:
